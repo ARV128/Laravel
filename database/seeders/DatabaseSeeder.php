7961f166bfa23db2a095431d04b866dae7457229
ARV128 <angelinavukovic1999@gmail.com>
2023-01-08 11:56:29 +0100

    dodat databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Klijent;
+use App\Models\Kupovina;
+use App\Models\Putovanje;
+use App\Models\User;
+use Database\Factories\PutovanjeFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +18,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Kupovina::truncate();
+        Putovanje::truncate();
+        Klijent::truncate();
+        User::truncate(); 
+       User::factory(3)->create();
+
+       Klijent::factory(15)->create();
+
+       Putovanje::factory(10)->create();
+       Kupovina::factory(10)->create();
     }
 }
